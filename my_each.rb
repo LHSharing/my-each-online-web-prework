ef0736620276fcecc7bd5a83eeb25308a62afc39
Lu Daley <daleyluis@gmail.com>
2019-01-27 17:08:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,7 +10,4 @@ end
 =begin
 You'll be building out the content of the #my_each method. This method should accept an argument of an array and use the while loop to iterate over each member of that array, yielding each element contained in the array to a block.
 Here's an example of what should happen when you call your my_each method:
-
-#   yield("#{n}")
- #  end 
 =end

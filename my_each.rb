e9a24685153fd1088486895d435844e653bf8a38
Lu Daley <daleyluis@gmail.com>
2019-01-27 17:06:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
-require 'pry'
 def my_each(words)
    counter = 0
    while counter < words.length
       yield(words[counter])
       counter += 1
    end
+   words.each {|n| puts n}
    words
 end
 =begin
